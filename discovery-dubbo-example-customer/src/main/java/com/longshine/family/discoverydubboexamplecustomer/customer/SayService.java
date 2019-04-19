7684bfa0e01d085a13da61540ec8c6be62e451b7
guojing762 <lemon123>
2019-04-19 11:49:32 +0800

    feat:add customer autoconfig and move dubbo service version into config

--- discovery-dubbo-example-customer/src/main/java/com/longshine/family/discoverydubboexamplecustomer/customer/SayService.java
+++ discovery-dubbo-example-customer/src/main/java/com/longshine/family/discoverydubboexamplecustomer/customer/SayService.java
@@ -1,17 +1,23 @@
 package com.longshine.family.discoverydubboexamplecustomer.customer;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.longshine.family.discoverydubboexamplecustomer.config.SelfConfigs;
 import com.longshine.family.discoverydubboexamplefacade.facade.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SayService {
 
-    @Reference(version = "1.0",check = false)
+    @Reference(version = "${demo.service.version}",check = false)
     DemoService demoService;
 
+    @Autowired
+    SelfConfigs selfConfigs;
+
     public String say(String name) {
-        return demoService.sayName(name);
+        String ss = selfConfigs.getUseLocalCache();
+        return demoService.sayName(name) +"：" +ss;
     }
 
 }
--- discovery-dubbo-example-provider/src/main/java/com/longshine/family/discoverydubboexampleprovider/config/SelfConfig.java
+++ discovery-dubbo-example-provider/src/main/java/com/longshine/family/discoverydubboexampleprovider/config/SelfConfig.java
@@ -4,10 +4,11 @@ import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * self configs
  * @Author: guojing
  * @Date: 2019/4/19 11:04
  */
 //@Configuration
-//@NacosPropertySource(dataId = "provider-configs.properties", autoRefreshed = true)
+//@NacosPropertySource(dataId = "your-xxx.properties", autoRefreshed = true)
 public class SelfConfig {
 }
